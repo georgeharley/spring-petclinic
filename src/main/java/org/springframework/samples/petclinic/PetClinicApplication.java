752f34ab512dc49d31fe7b975c06c1df846e9768
gharley <gharley@pivotal.io>
2018-07-11 18:28:19 +0100

    Retry datasource initialization if database not available
    * see https://github.com/spring-projects/spring-boot/issues/4779#issuecomment-305482782

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2014 the original author or authors.
+ * Copyright 2012-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,20 +16,79 @@
 
 package org.springframework.samples.petclinic;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.jdbc.datasource.AbstractDataSource;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.retry.annotation.Retryable;
 
 /**
  * PetClinic Spring Boot Application.
- * 
- * @author Dave Syer
  *
+ * @author Dave Syer
  */
 @SpringBootApplication
+@EnableRetry
 public class PetClinicApplication {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         SpringApplication.run(PetClinicApplication.class, args);
     }
 
+    /**
+     * Retry behaviour taken from https://github.com/spring-projects/spring-boot/issues/4779#issuecomment-305482782
+     */
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    private class RetryableDataSourceBeanPostProcessor implements BeanPostProcessor {
+        @Override
+        public Object postProcessBeforeInitialization(Object bean, String beanName)
+            throws BeansException {
+            if (bean instanceof DataSource) {
+                bean = new RetryableDataSource((DataSource) bean);
+            }
+            return bean;
+        }
+
+        @Override
+        public Object postProcessAfterInitialization(Object bean, String beanName)
+            throws BeansException {
+            return bean;
+        }
+    }
+
+    @Bean
+    public BeanPostProcessor dataSourceWrapper() {
+        return new RetryableDataSourceBeanPostProcessor();
+    }
+
+    class RetryableDataSource extends AbstractDataSource {
+
+        private DataSource delegate;
+
+        RetryableDataSource(DataSource delegate) {
+            this.delegate = delegate;
+        }
+
+        @Override
+        @Retryable(maxAttempts = 10, backoff = @Backoff(multiplier = 2.3, maxDelay = 30000))
+        public Connection getConnection() throws SQLException {
+            return delegate.getConnection();
+        }
+
+        @Override
+        @Retryable(maxAttempts = 10, backoff = @Backoff(multiplier = 2.3, maxDelay = 30000))
+        public Connection getConnection(String username, String password)
+            throws SQLException {
+            return delegate.getConnection(username, password);
+        }
+    }
 }
